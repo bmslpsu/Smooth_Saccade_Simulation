c22114115d140ec8dc05b1c7e0364204d86adb23
unknown <bud99@psu.edu>
2020-08-31 23:43:43 -0700

    Completed and verified against previous work.  Only very small discrepancies.

--- Reproduce Land‘s Model/a/fig_a_sim.m
+++ Reproduce Land‘s Model/a/fig_a_sim.m
@@ -29,6 +29,10 @@ Eh_pos = cumsum(E_p(N_temp+1:2*N_temp))/cumsum(I_p(N_temp+1:2*N_temp));
 El_vel = cumsum(E_v(1:N_temp))/cumsum(abs(I_v(1:N_temp)));
 Eh_vel = cumsum(E_v(N_temp+1:2*N_temp))/cumsum(abs(I_v(N_temp+1:2*N_temp)));
 
+asim_O_p = O_p;
+asim_E_p = E_p;
+save('a_sim_data.mat','asim_O_p','asim_E_p');
+
 % PLOT
 figure('units','normalized','outerposition',[0 0 0.5 1])
 
