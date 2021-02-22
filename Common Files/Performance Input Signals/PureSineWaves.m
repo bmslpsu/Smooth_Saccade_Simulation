232911351327bfc174b3d295a2146020f72db361
unknown <bud99@psu.edu>
2021-02-21 22:43:53 -0800

    Checkin of optimization work on hybrid performance.

---
+++ Common Files/Performance Input Signals/PureSineWaves.m
@@ -0,0 +1,18 @@
+scale = 50;
+pureSinTime = 0:0.001:5;
+sinfreqs = 0.5:0.5:5; %Hz
+pureSin = zeros(length(sinfreqs),length(pureSinTime));
+for i = 1:length(sinfreqs)
+    for j = 1:length(pureSinTime)
+        pureSin(i,j) = scale*sin(2*pi*sinfreqs(i)*pureSinTime(j));
+    end
+end
+
+figure
+plot(pureSinTime,pureSin(1,:),pureSinTime,pureSin(end,:),pureSinTime,pureSin(round(end/2),:))
+
+
+basepath = strcat(commonPath,'\Performance Input Signals');
+savePath = genFileName(basepath,'pureSin');
+save(savePath,'scale','pureSinTime','pureSin','sinfreqs')              
+    
